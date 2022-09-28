e0725329294a826561121f86543192fcf55d5625
almezj <al,mezj01@google.com>
2022-09-28 13:41:06 +0100

    Basic css

---
+++ script.js
@@ -0,0 +1,23 @@
+let addItemButton = document.getElementById("addItemButton")
+let listContainer = document.getElementById("todolist")
+let inputField = document.getElementById("nextThing")
+
+addItemButton.addEventListener('click', function(){
+	var listitem = document.createElement('li')
+	listitem.innerText = inputField.value
+	listitem.classList.add('styling')
+	listContainer.appendChild(listitem);
+	
+	listitem.addEventListener('click', function(){
+		if (listitem.style.textDecoration == ""){
+			listitem.style.textDecoration = "line-through";
+		}
+		else {
+			listitem.style.textDecoration = "";
+		}
+		
+	})
+	listitem.addEventListener("dblclick", function(){
+		listContainer.removeChild(listitem)
+	})
+})
